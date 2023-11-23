3b2493c87e822b9ec48c8d460c07936059395879
Joao Lucas <joaolribeiro0811@gmail.com>
2023-11-23 15:38:16 -0300

    Custom behavior

--- src/routes/Invoices/index.tsx
+++ src/routes/Invoices/index.tsx
@@ -1,6 +1,7 @@
 import './styles.css';
-import { NavLink, Outlet, useSearchParams } from "react-router-dom";
+import { Outlet, useSearchParams } from "react-router-dom";
 import { getInvoices } from "../../data";
+import QueryLink from '../../components/QueryLink';
 
 export default function Invoices() {
 
@@ -37,13 +38,14 @@ export default function Invoices() {
                         return invoiceName.startsWith(name.toLowerCase());
                     })
                     .map((invoice) => (
-                        <NavLink
-                            className={({ isActive }) => isActive ? "dblock nav-red" : "dblock nav-blue"}
+                        <QueryLink
+                            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                            className={({ isActive } : any) => isActive ? "dblock nav-red" : "dblock nav-blue"}
                             to={`/invoices/${invoice.number}`}
                             key={invoice.number}
                         >
                             {invoice.name}
-                        </NavLink>
+                        </QueryLink>
                     ))}
             </nav>
             <Outlet />
